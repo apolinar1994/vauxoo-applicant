99b534d022bf3507f6debbee780de2437146eb4c
apolinar1994 <sebastiaj043@gmail.com>
2016-04-20 00:46:11 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,27 @@
+ #!/usr/bin/python
+ # -*- coding: iso-8859-15 -*-
 """
-Your module documentation here
-"""
-
-
-class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
-
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+Crear un programa que dada la lista de numeros, obtener la suma del mismo
+ """
+import os, sys
+class CalculatorClass():
+   lista = []
+   suma = 0
+   def llenarlista(self):
+       x= int (raw_input("Ingrese Tamaño De La Lista:")) #Ingresar tamanio de la lista
+       for i in range(x):
+           
+           self.lista.append(int(raw_input("Ingrese Numero:")))   #llenar lista
+       
+       for i in self.lista:#imprimir lista
+           print i
+ 
+   def sum(self):
+       for i in self.lista:
+           self.suma+= i
+       print "La Suma De Los Datos Es:",self.suma
+ 
+obj = CalculatorClass()
+obj.llenarlista()
+obj.sum()
+			
